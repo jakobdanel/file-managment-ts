8fe2e7d5204e6f0bfdd15748a7d001e84112b8b6
Jakob <jakobdanel@aol.com>
2023-01-23 13:40:38 +0100

    Add test for assertInteger

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,5 +1,5 @@
 import "jest"
-import {assert, assertNotNegative} from "./utils";
+import {assert, assertInteger, assertNotNegative} from "./utils";
 
 describe("assert()", () => {
 
@@ -31,5 +31,22 @@ describe("assertNotNegative()", () => {
         expect(() => assertNotNegative(-0.00000000001)).toThrow();
         expect(() => assertNotNegative(NaN)).toThrow();
         expect(() => assertNotNegative(-Infinity)).toThrow();
+    })
+});
+
+describe("assertInteger()", () => {
+
+    it("should normally run if the number is an integer",() => {
+        assertInteger(1);
+        assertInteger(1000000000);
+        assertInteger(-256);
+    })
+
+    it("should throw an error if the number is not an integer",() => {
+        expect(() => assertInteger(2.4)).toThrow();
+        expect(() => assertInteger(-1.4)).toThrow();
+        expect(() => assertInteger(1.000000000000001)).toThrow();
+        expect(() => assertInteger(NaN)).toThrow();
+        
     })
 });
