0996beb8d49fcf25f2baab2dabc91b672ca25640
Jakob <jakobdanel@aol.com>
2023-01-23 13:35:49 +0100

    Add test for AssertNotNegative

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,7 +1,7 @@
 import "jest"
-import {assert} from "./utils";
+import {assert, assertNotNegative} from "./utils";
 
-describe("assert", () => {
+describe("assert()", () => {
 
     it("should normally return if the condition is true", () => {
         assert(true,"");
@@ -14,4 +14,22 @@ describe("assert", () => {
     it("should throw an error with the correct error message, if the condition is not true", () => {
         expect(() => assert(false,"This is a message")).toThrow("This is a message");
     });
+});
+
+describe("assertNotNegative()", () => {
+
+    it("should normally return if the number is not negative", () => {
+        assertNotNegative(1);
+        assertNotNegative(1000000000);
+        assertNotNegative(.0000001);
+        assertNotNegative(0);
+        assertNotNegative(Infinity)
+    });
+
+    it("should throw an error if the number is negative",() => {
+        expect(() => assertNotNegative(-1)).toThrow();
+        expect(() => assertNotNegative(-0.00000000001)).toThrow();
+        expect(() => assertNotNegative(NaN)).toThrow();
+        expect(() => assertNotNegative(-Infinity)).toThrow();
+    })
 });
