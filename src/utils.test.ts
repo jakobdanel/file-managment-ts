d93fc0fd3f4cea75f11a699a709b1b66657780bf
Jakob <jakobdanel@aol.com>
2023-01-23 13:47:04 +0100

    Add test for generateRadnomCharacters

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,18 +1,18 @@
 import "jest"
-import {assert, assertInteger, assertNotNegative} from "./utils";
+import { assert, assertInteger, assertNotNegative, generateRandomCharacters } from "./utils";
 
 describe("assert()", () => {
 
     it("should normally return if the condition is true", () => {
-        assert(true,"");
+        assert(true, "");
     });
 
     it("should throw an error if the condition is false", () => {
-        expect(() => assert(false,"")).toThrow();
+        expect(() => assert(false, "")).toThrow();
     });
 
     it("should throw an error with the correct error message, if the condition is not true", () => {
-        expect(() => assert(false,"This is a message")).toThrow("This is a message");
+        expect(() => assert(false, "This is a message")).toThrow("This is a message");
     });
 });
 
@@ -26,7 +26,7 @@ describe("assertNotNegative()", () => {
         assertNotNegative(Infinity)
     });
 
-    it("should throw an error if the number is negative",() => {
+    it("should throw an error if the number is negative", () => {
         expect(() => assertNotNegative(-1)).toThrow();
         expect(() => assertNotNegative(-0.00000000001)).toThrow();
         expect(() => assertNotNegative(NaN)).toThrow();
@@ -36,17 +36,48 @@ describe("assertNotNegative()", () => {
 
 describe("assertInteger()", () => {
 
-    it("should normally run if the number is an integer",() => {
+    it("should normally run if the number is an integer", () => {
         assertInteger(1);
         assertInteger(1000000000);
         assertInteger(-256);
     })
 
-    it("should throw an error if the number is not an integer",() => {
+    it("should throw an error if the number is not an integer", () => {
         expect(() => assertInteger(2.4)).toThrow();
         expect(() => assertInteger(-1.4)).toThrow();
         expect(() => assertInteger(1.000000000000001)).toThrow();
         expect(() => assertInteger(NaN)).toThrow();
-        
+
+    })
+});
+
+describe("generateRandomCharacters()", () => {
+
+    const N = 100;
+
+    let exampleStrings: string[] = []
+
+    beforeEach(() => {
+        for (let i = 0; i < N; i++) {
+            exampleStrings.push(generateRandomCharacters(N));
+        }
+    })
+
+    afterEach(() => exampleStrings = []);
+
+    it("should have the correct number of characters", () => {
+        exampleStrings.forEach(example => {
+            expect(example.length).toEqual(N);
+        })
+    });
+
+    it("should have only lowercase alphabetical values", () => {
+        exampleStrings.forEach(example => {
+            for (let i = 0; i < example.length; i++) {
+                let char = example.charCodeAt(i);
+                expect(char).toBeGreaterThanOrEqual(97);
+                expect(char).toBeLessThanOrEqual(123);
+            }
+        })
     })
 });
